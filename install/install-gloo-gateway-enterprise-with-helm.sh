04f04eb9d0f0b78abaa7a03ede1205dff8fc0afd
Duncan Doyle <Duncan.Doyle@gmail.com>
2024-07-19 16:31:56 +0200

    Upgrade to Gloo Gateway 1.17 and K8S Gateway API.

---
+++ install/install-gloo-gateway-enterprise-with-helm.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+export GLOO_GATEWAY_VERSION="1.17.0"
+export GLOO_MESH_VERSION="2.6.0-rc1"
+
+export GLOO_GATEWAY_HELM_VALUES_FILE="gloo-gateway-helm-values.yaml"
+export GLOO_MESH_HELM_VALUES_FILE="gloo-mesh-helm-values.yaml"
+
+export GLOO_MESH_CLUSTER_NAME="gm-demo-single"
+
+if [ -z "$GLOO_GATEWAY_LICENSE_KEY" ]
+then
+   echo "Gloo Gateway License Key not specified. Please configure the environment variable 'GLOO_GATEWAY_LICENSE_KEY' with your Gloo Gateway License Key."
+   exit 1
+fi
+
+if [ -z "$GLOO_MESH_LICENSE_KEY" ]
+then
+   echo "Gloo Mesh License Key not specified. Please configure the environment variable 'GLOO_GATEWAY_LICENSE_KEY' with your Gloo Gateway License Key."
+   exit 1
+fi
+
+# You need to install the GME CRDs first, i.e. before installing Gloo GATEWAY. If not, the GME CRDs will fail to install.
+# See https://github.com/solo-io/gloo-mesh-enterprise/issues/14143
+
+printf "\nInstalling Gloo Mesh CRDs\n"
+helm upgrade --install gloo-platform-crds gloo-platform/gloo-platform-crds \
+   --namespace=gloo-mesh \
+   --create-namespace \
+   --version $GLOO_MESH_VERSION
+printf "\n"
+
+printf "\nCreate gloo-mesh-addons namespace if it does not yet exist.\n"
+kubectl create namespace gloo-mesh-addons --dry-run=client -o yaml | kubectl apply -f -
+
+printf "\nInstalling Gloo Mesh $GLOO_MESH_VERSION ...\n"
+helm upgrade --install gloo-platform gloo-platform/gloo-platform \
+   --namespace gloo-mesh \
+   --version $GLOO_MESH_VERSION \
+   --values $GLOO_MESH_HELM_VALUES_FILE \
+   --set common.cluster=$GLOO_MESH_CLUSTER_NAME \
+   --set licensing.glooMeshLicenseKey=$GLOO_MESH_LICENSE_KEY
+   # --set licensing.glooGatewayLicenseKey=$GLOO_GATEWAY_LICENSE_KEY \
+printf "\n"
+
+
+# #----------------------------------------- Install Gloo Gateway with K8S Gateway API support -----------------------------------------
+
+# printf "\nApply K8S Gateway CRDs ....\n"
+# kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+
+
+# printf "\nInstalling Gloo GATEWAY $GLOO_GATEWAY_VERSION ...\n"
+# helm upgrade --install gloo glooe/gloo-ee --namespace gloo-system --create-namespace --set-string license_key=$GLOO_GATEWAY_LICENSE_KEY -f $GLOO_GATEWAY_HELM_VALUES_FILE --version $GLOO_GATEWAY_VERSION
+# printf "\n"
+
+
+# pushd ../
+# #----------------------------------------- Deploy the Gateway -----------------------------------------
+
+# # create the ingress-gw namespace
+# kubectl create namespace ingress-gw --dry-run=client -o yaml | kubectl apply -f -
+
+# printf "\nDeploy the Gateway ...\n"
+# kubectl apply -f gateways/gw.yaml
+
+
+
+
+
+popd
--- install/setup.sh
+++ install/setup.sh
@@ -11,8 +11,28 @@
 
 pushd ../
 
+
+# # Gloo Gateway FDS Whitelist labels
+# kubectl label namespace default discovery.solo.io/function_discovery=enabled
+
+
+# kubectl label upstream -n myapp myupstream discovery.solo.io/function_discovery=enabled
+
+# # if the Upstream was discovered and is managed by UDS (Upstream Discovery Service)
+# # then you can add the label to the service and it will propagate to the Upstream
+# kubectl label service -n myapp myservice discovery.solo.io/function_discovery=enabled
+
+
+
 # Label the default namespace for Istio sidecar injection ...
-kubectl label namespace default istio-injection=enabled
+# kubectl label namespace default istio-injection=enabled
+export REVISION=$(kubectl get pod -L app=istiod -n istio-system -o jsonpath='{.items[0].metadata.labels.istio\.io/rev}')
+# echo $REVISION
+kubectl label ns default istio.io/rev=$REVISION --overwrite=true
+
+kubectl label ns default shared-gateway-access="true" --overwrite=true
+
+
 
 # Apply the HTTPBin sevice
 kubectl apply -f apis/httpbin.yaml
@@ -24,9 +44,9 @@ kubectl apply -f policies/istio/peer-authentication-policy.yaml
 kubectl apply -f virtualservices/api-example-com-vs.yaml
 
 # Configure mTLS on the HTTPBin Upstream,
-printf "\nConfiguring mTLS on default HTTPBin Upstream ...\n"
-sleep 2
-glooctl istio enable-mtls --upstream default-httpbin-8000
+# printf "\nConfiguring mTLS on default HTTPBin Upstream ...\n"
+# sleep 2
+# glooctl istio enable-mtls --upstream default-httpbin-8000
 # glooctl istio disable-mtls --upstream default-httpbin-8000
 
 popd
